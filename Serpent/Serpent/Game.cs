6f393ee8b61a4ee486201aa6dfa81705c9c478df
orabillon <115501074+orabillon@users.noreply.github.com>
2024-05-05 17:16:06 +0200

    Création du projet
    Découverte de Raylib et des fondamentaux

---
+++ Serpent/Serpent/Game.cs
@@ -0,0 +1,65 @@
+﻿using Raylib_CsLo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+using Texture2D = Raylib_CsLo.Texture;
+
+namespace Serpent
+{
+    public class Game
+    {
+        Vector2 position;
+
+        Texture2D texture;
+
+        int vitesse = 3;
+
+        public void Load() 
+        {
+            texture = Raylib.LoadTexture("images/ship.png");
+            position = new Vector2(50,50);
+        }
+
+        public void Update() 
+        {
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+            {
+               position.Y -= vitesse;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+            {
+                position.X -= vitesse;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+            {
+                position.Y += vitesse;
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+            {
+                position.X += vitesse;
+            }
+        }
+
+        public void Draw()
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Raylib.BLACK);
+
+            Raylib.DrawText("Congrats! You created your first window!", 19, 19, 20, Raylib.WHITE);
+
+            Raylib.DrawTextureEx(texture, position,20,1,Raylib.WHITE);
+
+            Raylib.EndDrawing();
+        }
+
+        public void Unload() 
+        {
+            Raylib.UnloadTexture(texture);
+        }
+    }
+}
---
+++ Serpent/Serpent/Program.cs
@@ -0,0 +1,38 @@
+﻿using Raylib_CsLo;
+using Texture2D = Raylib_CsLo.Texture;
+
+namespace Serpent
+{
+    public static class Program
+    {
+        public static Task Main(string[] args)
+        {
+            const int screenWidth = 800;
+            const int screenHeight = 450;
+
+            // creation fenetre 
+            Raylib.InitWindow(screenWidth, screenHeight, "Jeu du serpent");
+            Raylib.SetTargetFPS(60);
+
+            Game monJeu = new Game();
+
+            monJeu.Load();
+
+            // Game loop
+            while (!Raylib.WindowShouldClose())
+            {
+
+                // Update 
+                monJeu.Update();
+
+                // Draw 
+                monJeu.Draw();
+            }
+
+            monJeu.Unload();
+            Raylib.CloseWindow();
+
+            return Task.CompletedTask;
+        }
+    }
+}
