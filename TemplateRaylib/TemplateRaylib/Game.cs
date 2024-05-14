62c80e376bcd1732406d76f64afcd2b619456002
orabillon <115501074+orabillon@users.noreply.github.com>
2024-05-14 20:56:44 +0200

    Template Raylib
    -ajout ressource

---
+++ TemplateRaylib/TemplateRaylib/Game.cs
@@ -0,0 +1,32 @@
+﻿using Raylib_cs;
+
+namespace TemplateRaylib;
+
+public class Game
+{
+    public void Load()
+    {
+        
+    }
+
+    public void Update(float dt)
+    {
+        
+    }
+
+    public void Draw()
+    {
+        Raylib.BeginDrawing();
+
+        Raylib.ClearBackground(Color.White);
+
+        Raylib.DrawText("Congrats! You created your first window!", 190, 200, 20, Color.LightGray);
+
+        Raylib.EndDrawing();
+    }
+
+    public void Unload()
+    {
+        
+    }
+}
--- TemplateRaylib/TemplateRaylib/Program.cs
+++ TemplateRaylib/TemplateRaylib/Program.cs
@@ -1,37 +1,36 @@
 ﻿using Raylib_cs;
+using TemplateRaylib;
 
 // Initialization
 //--------------------------------------------------------------------------------------
 const int screenWidth = 800;
 const int screenHeight = 600;
 
+Game monJeu = new Game();
+
 Raylib.InitWindow(screenWidth, screenHeight, "raylib Template");
 
 Raylib.SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 //--------------------------------------------------------------------------------------
 
+// Permet de jouer des son
+Raylib.InitAudioDevice();
+
+// chargement des ressource
+    monJeu.Load();
+
 // Main game loop
 while (!Raylib.WindowShouldClose())    // Detect window close button or ESC key
 {
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
-    //----------------------------------------------------------------------------------
+    monJeu.Update(Raylib.GetFrameTime());
 
-    // Draw
-    //----------------------------------------------------------------------------------
-    Raylib.BeginDrawing();
-
-    Raylib.ClearBackground(Color.White);
-
-    Raylib.DrawText("Congrats! You created your first window!", 190, 200, 20, Color.LightGray);
-
-    Raylib.EndDrawing();
-    //----------------------------------------------------------------------------------
+    monJeu.Draw();
 }
 
 // De-Initialization
 //--------------------------------------------------------------------------------------
+monJeu.Unload();
+Raylib.CloseAudioDevice();
 Raylib.CloseWindow();        // Close window and OpenGL context
 //--------------------------------------------------------------------------------------
 
