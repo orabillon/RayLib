e74c0a3d744d4c5737c21a88e9820baf8d97ce22
orabillon <115501074+orabillon@users.noreply.github.com>
2024-06-01 21:15:13 +0200

    Reorganisation dossier projet raylib
    - création dossier pour dev C# et Dev C
    - Déplacement des projet dans nouvelle architecture

---
+++ C/Template/build.sh
@@ -0,0 +1,20 @@
+#/usr/bin/env bash
+
+rm ./bin/MonProg
+rm ./bin/assets/images/*.*
+rm ./bin/assets/fonts/*.*
+rm ./bin/assets/sons/*.*
+
+cp ./src/assets/images/*.* ./bin/assets/images/
+cp ./src/assets/fonts/*.* ./bin/assets/fonts/
+cp ./src/assets/sons/*.* ./bin/assets/sons/
+
+gcc -Wall -Werror -pedantic -g src/main.c -o bin/MonProg -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 
+
+if [ $? -eq 0 ]; then
+    echo "Compilation OK"
+    cd bin
+    ./MonProg
+else
+    echo "Erreur de compilation"
+fi
